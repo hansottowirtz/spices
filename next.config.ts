841765b87330a47c3ce10d13da8c9c4ff006f36c
Hans Otto Wirtz <hansottowirtz@gmail.com>
2025-12-26 19:31:37 +0100

    chore: upgrade next

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */  
-  eslint: {
-    ignoreDuringBuilds: true
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
